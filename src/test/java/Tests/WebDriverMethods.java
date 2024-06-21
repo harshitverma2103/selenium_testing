7cc35f51ecb3f40719057560593de4025396eb98
harshitverma2103 <harshitverma2103@gmail.com>
2024-06-21 11:35:16 +0530

    implement navigate methods of webdriver

--- src/test/java/Tests/WebDriverMethods.java
+++ src/test/java/Tests/WebDriverMethods.java
@@ -14,14 +14,15 @@ public class WebDriverMethods {
 
           // get()
           driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+
           // getTitle()
           String title = driver.getTitle();
           System.out.println(title);
 
           // getCurrentUrl()
 
-          String currentPageUrl = driver.getCurrentUrl();
-          System.out.println(currentPageUrl);
+          // String currentPageUrl = driver.getCurrentUrl();
+          // System.out.println(currentPageUrl);
 
           // getPageSource()
 
@@ -31,6 +32,24 @@ public class WebDriverMethods {
           // getWindowHadle()
           String window = driver.getWindowHandle();
           System.out.println(window);
+
+          /* navigational methods */
+
+          // navigate().to()
+
+          driver.navigate().to("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+          driver.navigate().to("https://opensource-demo.orangehrmlive.com/web/index.php/auth/requestPasswordResetCode");
+          // navigate().refresh()
+          driver.navigate().refresh();
+          // Add sleep for 5 seconds
+          try {
+               Thread.sleep(5000);
+          } catch (InterruptedException e) {
+               e.printStackTrace();
+          }
+
+          // navigate().back()
+          driver.navigate().back();
 
           driver.quit();
      }
